38f7574ac7880ec222ac8f010d7a055ee7fd0da7
Laur <Laur>
2017-03-24 09:19:25 +0200

    captcha

---
+++ web/solutii/laur.ciucanu/captcha/captcha.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<p> Ecuatia: 9x+3=21 </p>
+<form action="interpret.php" method="get">
+	Raspuns: <input type="text" name="name"><br>
+<input type="submit">
+
+</form>
+
+</body>
+</html>
